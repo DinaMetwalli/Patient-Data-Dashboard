d07d7e1c55a6fd1e38ef2a8bbcfc00f17b63430f
Dina Metwalli <dina.moh657@gmail.com>
2024-03-17 20:42:12 +0000

    fix(database): change port to default

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,4 +3,4 @@ import psycopg2
 
 def test_underlying_pg_connection():
     psycopg2.connect(
-        "dbname=postgres user=postgres password=postgres host=localhost port=5433")
+        "dbname=postgres user=postgres password=postgres host=localhost port=5432")
