f74b453dc1b75fb76f30b0704d20a8472f15d210
Dina Metwalli <dina.moh657@gmail.com>
2024-03-19 22:31:35 +0000

    feat: add class for handling errors

---
+++ src/utils/errors.py
@@ -0,0 +1,16 @@
+class RaiseError(Exception):
+    """Base class for all system errors."""
+
+    def __init__(self, message: str) -> None:
+        """Create error message."""
+        self.message = message
+
+
+class AuthorizationError(RaiseError):
+    """Error for when a user has incorrect access credentials."""
+
+
+class InputError(RaiseError):
+    """
+    Error for when an invalid input is provided.
+    """
