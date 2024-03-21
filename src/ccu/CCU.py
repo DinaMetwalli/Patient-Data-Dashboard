83c22cf74ede259339d38043d83d033291269769
Dina Metwalli <dina.moh657@gmail.com>
2024-03-21 14:28:43 +0000

    feat(ccu): add csv import check

--- src/ccu/CCU.py
+++ src/ccu/CCU.py
@@ -10,3 +10,8 @@ class CCU():
         if not self.auth.validate_entry_passkey(passkey):
             raise AuthorizationError("Incorrect Passkey")
         return True
+    
+    def import_password(self,password: str) -> None:
+        if not self.auth.validate_import_password(password):
+            raise AuthorizationError("Incorrect Import Password")
+        return True
