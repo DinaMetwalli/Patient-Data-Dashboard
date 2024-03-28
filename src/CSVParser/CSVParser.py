0732fc3080bdfd5a1b28a34e4eea9e45878aa6b1
Beyzanrkyc <beyzanurkyc.bk@gmail.com>
2024-03-28 15:02:36 +0000

    feat(parser): add csv parser

---
+++ src/CSVParser/CSVParser.py
@@ -0,0 +1,45 @@
+import csv
+import os
+import pandas as pd
+
+class ParseCSV():
+    _instance = None
+    
+    def __init__(self):
+        self.data = []
+        self.features = []
+
+    @classmethod
+    def get_instance(cls, ccu):
+        if cls._instance is None:
+            cls._instance = cls(ccu)
+        return cls._instance
+    
+    def import_csv(self):
+        file_path = "Data.csv"
+        if os.path.exists(file_path):
+            # Load the CSV file
+            self.data = pd.read_csv(file_path)
+            print("CSV file imported successfully.")
+        else:
+            print("File not found.")
+
+    def export_csv(self):
+        try:
+            if self.data is not None:
+                print(self.data)
+                # Export DataFrame to CSV
+                self.data.to_csv("ExportedData.csv", index=False)
+                print("CSV file exported successfully.")
+            else:
+                print("No data to export.") 
+        except Exception as e:
+            print("An error occurred while exporting CSV:", e)
+
+
+# Create an instance of the class
+parser = ParseCSV()
+
+# Call the import_csv method
+parser.import_csv()
+parser.export_csv()
