4f2971f1933edc51960fcdd9ba27ef8a6774fab6
Dina Metwalli <dina.moh657@gmail.com>
2024-04-05 14:35:38 +0100

    feat(api): make api simpler

--- example/src/main.py
+++ example/src/main.py
@@ -1,18 +1,21 @@
-from flask import Flask
+from flask import Flask, request
 from .api.utils.FSrouter import FSRouter
+from example.src.api.routes.calculate.data import post
 
 def main():
 
     print("Flask API running..!")
     app: Flask = Flask("Feeding Dashboard")
 
-    router = FSRouter(app)
-    router.load_dir("example\\src\\api\\routes")
-
     @app.route('/')
-    
     def index():
         return "<p>Testing</p>"
+    
+    @app.route('/calculate/data', methods=['POST'])
+    def calculate():
+        request_data = request.json
+        response, status_code = post(request_data)
+        return response, status_code
     
     print("Starting the server...")
     app.run(debug=True, port=6002, host="0.0.0.0")
