740969bf425e7fa28c4971ed13efd4cc5cdf20e8
Dina Metwalli <dina.moh657@gmail.com>
2024-04-05 15:29:31 +0100

    docs(example): add comments for js communication
    - remove unnecessary IPC handler
    - explain how js allows communication between
    backend and frontend/

--- example/electron/main.js
+++ example/electron/main.js
@@ -10,20 +10,13 @@ function createWindow() {
         }
     });
 
-    // removes the default electron top menu bar:
+    // Remove the default electron top menu bar:
     mainWindow.setMenuBarVisibility(false);
 
     mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
     mainWindow.on('closed', function () {
         app.quit();
-    });
-
-    // Define IPC handler for endpoint
-    ipcMain.handle('calculate-data', async (event, { num1, num2 }) => {
-        // Handle the calculation and return the result
-        const result = num1 + num2;
-        return { result };
     });
 }
 
@@ -34,9 +27,3 @@ app.on('window-all-closed', function () {
         app.quit();
     }
 });
-
-// when you create the desktop run.bat application thing that opens the dashboard, you also have to make sure
-// that on first time you open it, it runs the flask server and then waits for flask to start and then opens,
-// then you have 1 of two options. 1: when you close it it also closes the flask server. 2: when you close it
-// and go to open it again, it checks if the flask server is already running or not, and if it is, it doesn't
-// start it, but instead instantly starts the electron app. 
