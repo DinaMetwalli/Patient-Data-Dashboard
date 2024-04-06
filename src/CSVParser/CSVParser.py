e8972e72a3896c2aa0a163a09919cbc38f32dd09
Dina Metwalli <dina.moh657@gmail.com>
2024-04-06 17:58:21 +0100

    refactor(parser): change empty cells to None

--- src/CSVParser/CSVParser.py
+++ src/CSVParser/CSVParser.py
@@ -1,6 +1,7 @@
 import csv
 import os
 import pandas as pd
+import numpy as np
 
 class ParseCSV():
     _instance = None
@@ -13,34 +14,33 @@ class ParseCSV():
     def get_instance(cls, ccu):
         if cls._instance is None:
             cls._instance = cls(ccu)
+
         return cls._instance
     
-    def import_csv(self):
-        file_path = "src/CSVParser/Data.csv"
+    def import_csv(self, import_name: str) -> pd.DataFrame | None:
+        file_path = f"src/CSVParser/{import_name}"
+        
         if os.path.exists(file_path):
             # Load the CSV file
             self.data = pd.read_csv(file_path)
-            self.data.fillna(value="", inplace=True)
+            self.data = self.data.replace(np.nan, None)
+
             print("CSV file imported successfully.")
+            
+            return self.data
+        
         else:
             print("File not found.")
 
-    def export_csv(self):
+    def export_csv(self, export_name: str) -> None:
         try:
             if self.data is not None:
-                print(self.data)
                 # Export DataFrame to CSV
-                self.data.to_csv("ExportedData.csv", index=False)
+                self.data.to_csv(export_name, index=False)
+
                 print("CSV file exported successfully.")
+            
             else:
                 print("No data to export.") 
         except Exception as e:
             print("An error occurred while exporting CSV:", e)
-
-
-# Create an instance of the class
-parser = ParseCSV()
-
-# Call the import_csv method
-parser.import_csv()
-parser.export_csv()
