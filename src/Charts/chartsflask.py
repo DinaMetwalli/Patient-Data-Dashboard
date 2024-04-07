224b7e3046a0793d5279d76309146c24e67cee1a
Serhii <117412985+cas7om@users.noreply.github.com>
2024-04-07 20:26:10 +0100

    fix(charts):4 different charts in electron

--- src/Charts/chartsflask.py
+++ src/Charts/chartsflask.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from CSVParser import ParseCSV #need to have it  like this till our main app won't be in another folder
+from CSVParser import ParseCSV  # Assuming ParseCSV is correctly defined
 
 app = Flask(__name__)
 parser = ParseCSV()
@@ -11,12 +11,27 @@ def index():
 @app.route("/import-csv", methods=["POST"])
 def import_csv():
     try:
-        import_name = "csvfile.csv"  # Corrected the variable assignment
+        import_name = "csvfile.csv"  
         parser.import_csv(import_name)
-        data = {
+        data = { #The prob  
             "encounterId": parser.data["encounterId"].tolist(),
             "end_tidal_co2": parser.data["end_tidal_co2"].tolist(),
-            "referral": parser.data["referral"].tolist(),
+            "feed_vol": parser.data["feed_vol"].tolist(),
+            "feed_vol_adm": parser.data["feed_vol_adm"].tolist(),
+            "fio2": parser.data["fio2"].tolist(),
+            "fio2_ratio": parser.data["fio2_ratio"].tolist(),
+            "insp_time": parser.data["insp_time"].tolist(),
+            "oxygen_flow_rate": parser.data["oxygen_flow_rate"].tolist(),
+            "peep": parser.data["peep"].tolist(),
+            "pip": parser.data["pip"].tolist(),
+            "resp_rate": parser.data["resp_rate"].tolist(),
+            "sip": parser.data["sip"].tolist(),
+            "tidal_vol": parser.data["tidal_vol"].tolist(),
+            "tidal_vol_actual": parser.data["tidal_vol_actual"].tolist(),
+            "tidal_vol_kg": parser.data["tidal_vol_kg"].tolist(),
+            "tidal_vol_spon": parser.data["tidal_vol_spon"].tolist(),
+            "bmi": parser.data["bmi"].tolist(),
+            "referral": parser.data["referral"].tolist()
         }
         return jsonify({"success": True, "data": data})
     except Exception as e:
--- src/Charts/electron/charts.js
+++ src/Charts/electron/charts.js
@@ -1,38 +1,57 @@
-async function importCSV() {
-    try {
-        const response = await fetch('http://localhost:6002/import-csv', {
-            method: 'POST'
-        });
-
-        if (!response.ok) {
-            throw new Error('Failed to fetch');
-        }
-
-        const data = await response.json();
-        console.log("Data received:", data);
-
-        if (data.success) {
-            buildGraphs(data.data);
-            showResult(`CSV Imported successfully.`);
-        } else {
-            console.error("Failed to import CSV:", data.message);
-            showResult(`Failed to import CSV: ${data.message}`);
-        }
-    } catch (error) {
-        console.error("An error occurred:", error);
-        showResult(`An error occurred: ${error.message}`);
-    }
+function calculateAverage(data) {
+    const filteredData = data.filter(value => value !== null && !isNaN(value));
+    const sum = filteredData.reduce((acc, curr) => acc + curr, 0);
+    return sum / filteredData.length;
 }
 
-function showResult(result) {
-    const resultElement = document.getElementById("result");
-    resultElement.textContent = `Result: ${result}`;
+function calculateRespiratoryMeasurementsAverage(data) {
+    console.log("Calculating Respiratory Measurements Average...");
+
+    const averages = {
+        "FiO2": calculateAverage(data.fio2),
+        "FiO2 Ratio": calculateAverage(data.fio2_ratio),
+        "Oxygen Flow Rate": calculateAverage(data.oxygen_flow_rate),
+        "Respiratory Rate": calculateAverage(data.resp_rate),
+        "SIP": calculateAverage(data.sip)
+    };
+
+    console.log('Respiratory Measurements Average:', averages);
+    return averages;
 }
 
-function buildGraphs(data) {
-    console.log("Building graphs with data:", data);
-    // Build the pie chart for referral statistics
-    var referralData = data.referral;
+function calculateMechanicalVentillationAverage(data) {
+    console.log("Calculating Mechanical Ventillation Average...");
+
+    const averages = {
+        "ETCO2": calculateAverage(data.end_tidal_co2),
+        "PEEP": calculateAverage(data.peep),
+        "PIP": calculateAverage(data.pip),
+        "Tidal Volume": calculateAverage(data.tidal_vol),
+        "Actual Tidal Volume": calculateAverage(data.tidal_vol_actual),
+        "Tidal Volume per kg": calculateAverage(data.tidal_vol_kg),
+        "Spontaneous Tidal Volume": calculateAverage(data.tidal_vol_spon),
+        "Inspiration Time": calculateAverage(data.insp_time)
+    };
+
+    console.log('Mechanical Ventillation Average:', averages);
+    return averages;
+}
+
+function calculateDietaryRequirementsAverage(data) {
+    console.log("Calculating Dietary Requirements Average...");
+
+    const averages = {
+        "FV": calculateAverage(data.feed_vol),
+        "FV Administered": calculateAverage(data.feed_vol_adm),
+        "BMI": calculateAverage(data.bmi)
+    };
+
+    console.log('Dietary Requirements Average:', averages);
+    return averages;
+}
+
+
+function buildPieChart(referralData) {
     var referredCount = referralData.filter(value => value === 1 || isNaN(value)).length;
     var notReferredCount = referralData.filter(value => value === 0 || isNaN(value)).length;
 
@@ -40,8 +59,8 @@ function buildGraphs(data) {
         labels: ['Referred', 'Not Referred'],
         datasets: [{
             data: [referredCount, notReferredCount],
-            backgroundColor: ['#36A2EB', '#FF6384'],
-            hoverBackgroundColor: ['#36A2EB', '#FF6384']
+            backgroundColor: ['#1a2a6c', '#fdbb2d'], // Deep blues and golden yellow
+            hoverBackgroundColor: ['#1a2a6c', '#fdbb2d']
         }]
     };
 
@@ -69,7 +88,132 @@ function buildGraphs(data) {
     });
 }
 
+function buildRespiratoryMeasurementsGraph(averages) {
+    const labels = Object.keys(averages);
+    const values = Object.values(averages);
 
+    const ctx = document.getElementById('respiratoryMeasurementsChart').getContext('2d');
+    const respiratoryMeasurementsChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Respiratory Measurements Average',
+                data: values,
+                backgroundColor: [
+                    '#1a2a6c', '#fdbb2d', '#1a2a6c', '#fdbb2d', '#1a2a6c' // Alternating deep blues and golden yellow
+                ],
+                borderColor: [
+                    '#1a2a6c', '#fdbb2d', '#1a2a6c', '#fdbb2d', '#1a2a6c'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+function buildMechanicalVentillationGraph(averages) {
+    const labels = Object.keys(averages);
+    const values = Object.values(averages);
+
+    const ctx = document.getElementById('mechanicalVentillationChart').getContext('2d');
+    const mechanicalVentillationChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Mechanical Ventillation Average',
+                data: values,
+                backgroundColor: [
+                    '#1a2a6c', '#fdbb2d', '#1a2a6c', '#fdbb2d', '#1a2a6c', '#fdbb2d', '#1a2a6c', '#fdbb2d' // Alternating deep blues and golden yellow
+                ],
+                borderColor: [
+                    '#1a2a6c', '#fdbb2d', '#1a2a6c', '#fdbb2d', '#1a2a6c', '#fdbb2d', '#1a2a6c', '#fdbb2d'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+function buildDietaryRequirementsGraph(averages) {
+    const labels = Object.keys(averages);
+    const values = Object.values(averages);
+
+    const ctx = document.getElementById('dietaryRequirementsChart').getContext('2d');
+    const dietaryRequirementsChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Dietary Requirements Average',
+                data: values,
+                backgroundColor: [
+                    '#1a2a6c', '#fdbb2d', '#1a2a6c' // Deep blues and golden yellow
+                ],
+                borderColor: [
+                    '#1a2a6c', '#fdbb2d', '#1a2a6c'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+}
+
+async function importCSV() {
+    try {
+        const response = await fetch('http://localhost:6002/import-csv', {
+            method: 'POST'
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch');
+        }
+
+        const data = await response.json();
+        console.log("Data received:", data);
+
+        if (data.success) {
+            buildPieChart(data.data.referral);
+            buildRespiratoryMeasurementsGraph(calculateRespiratoryMeasurementsAverage(data.data));
+            buildMechanicalVentillationGraph(calculateMechanicalVentillationAverage(data.data));
+            buildDietaryRequirementsGraph(calculateDietaryRequirementsAverage(data.data));
+            showResult(`CSV Imported successfully.`);
+        } else {
+            console.error("Failed to import CSV:", data.message);
+            showResult(`Failed to import CSV: ${data.message}`);
+        }
+    } catch (error) {
+        console.error("An error occurred:", error);
+        showResult(`An error occurred: ${error.message}`);
+    }
+}
+
+function showResult(result) {
+    const resultElement = document.getElementById("result");
+    resultElement.textContent = `Result: ${result}`;
+}
 
 // Call the importCSV function when the page loads
 document.addEventListener("DOMContentLoaded", function() {
