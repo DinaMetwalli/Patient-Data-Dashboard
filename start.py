a21b3ed8dd86a67d4455e9b5970eaad2228fecbf
Dina Metwalli <dina.moh657@gmail.com>
2024-04-08 21:53:52 +0100

    fix: remove redundant example files

--- start.py
+++ start.py
@@ -2,14 +2,21 @@ from example.src.main import main
 from src.Reports.ReportGenerator import Report
 from src.Reports.PatientReport import PatientReport
 from src.Reports.AverageReport import AverageReport
+from src.Patients.Patients import Patient
 
 # Start server:
 # main()
 
 # # Generate average data report:
 # instance = AverageReport()
-# instance.generate_average_data_report("Data.csv", "Huh.pdf")
+# instance.generate_average_data_report("Data.csv", "Average_data.pdf")
 
 # # Generate patient report:
 # rep = PatientReport()
 # rep.generate_patient_data_report(2906, "Data.csv", "Patient_data.pdf")
+
+# # Fetch Patient's Dietary Requirements Data:
+# patient = Patient(2956, "Data.csv")
+# dietary_data = patient.dietary_req()
+# data = dietary_data.fetch_dietary_req_data()
+# print(data)
