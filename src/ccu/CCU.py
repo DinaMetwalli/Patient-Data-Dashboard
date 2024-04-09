852de0f01976c93b98840213385f66a0ff9db0bc
Dina Metwalli <dina.moh657@gmail.com>
2024-04-09 21:41:38 +0100

    feat(ccu): add entry method

---
+++ src/ccu/CCU.py
@@ -0,0 +1,12 @@
+from .Authenticator import Authenticator
+from ..utils.errors import AuthorizationError
+
+class CCU():
+    
+    def __init__(self):
+        self.auth = Authenticator()
+
+    def entry(self, passkey: str) -> bool:
+        if not self.auth.validate_entry_passkey(passkey):
+            raise AuthorizationError("Incorrect Passkey")
+        return True
