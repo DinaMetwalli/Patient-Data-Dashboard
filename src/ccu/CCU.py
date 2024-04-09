0747a12196f1c8df58f9ef6b94a9786f5009c3ad
Dina Metwalli <dina.moh657@gmail.com>
2024-04-09 22:56:34 +0100

    feat(ccu): add references to other classes

--- src/ccu/CCU.py
+++ src/ccu/CCU.py
@@ -1,5 +1,8 @@
 from .Authenticator import Authenticator
 from ..utils.errors import AuthorizationError
+from ..CSVParser.CSVParser import ParseCSV
+from ..Patients.Patients import Patient
+from ..Reports.ReportGenerator import Report
 
 class CCU():
     
@@ -20,3 +23,12 @@ class CCU():
         if not self.auth.validate_export_password(password):
             raise AuthorizationError("Incorrect Export Password")
         return True
+    
+    def csv_parser(self) -> ParseCSV:
+        return ParseCSV()
+    
+    def patients(self) -> Patient:
+        return Patient
+    
+    def reports(self) -> Report:
+        return Report
