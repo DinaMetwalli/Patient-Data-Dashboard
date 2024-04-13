028e78952f3f847489934e70a5356aecc5af9f3c
Dina Metwalli <dina.moh657@gmail.com>
2024-04-13 20:35:19 +0100

    feat(api): add average reports to GUI

--- electron/reports.js
+++ electron/reports.js
@@ -1,17 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const generateButton = document.getElementById('patient_report');
+    const generatePatientButton = document.getElementById('patient_report');
+    const generateAverageButton = document.getElementById('average_report');
 
     const IDInput = document.getElementById('patient_id');
-    const exportInput = document.getElementById('export_name');
+    const exportPatient = document.getElementById('export_patient');
+    const exportAverage = document.getElementById('export_average');
 
-    generateButton.addEventListener('click', async () => {
+    generatePatientButton.addEventListener('click', async () => {
 
         const patient_id = parseInt(IDInput.value);
-        const export_name = exportInput.value;
+        const export_name = exportPatient.value;
 
         try {
-            const response = await fetch('http://localhost:6002/patient', {
+            const response = await fetch('http://localhost:6002/patient-report', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
@@ -27,4 +29,27 @@ document.addEventListener('DOMContentLoaded', () => {
             console.error('Error generating report:', error);
         }
     });
+
+    generateAverageButton.addEventListener('click', async () => {
+
+        const export_name = exportAverage.value;
+
+        try {
+            const response = await fetch('http://localhost:6002/average-report', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ export_name })
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to generate report');
+            }
+
+        } catch (error) {
+            console.error('Error generating report:', error);
+        }
+    });
 });
+
