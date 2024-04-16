cf628068e470419250dca442b6c61e202f8ecab3
Beyzanrkyc <beyzanurkyc.bk@gmail.com>
2024-04-16 01:23:46 +0100

    remove session_data.json file

--- start.py
+++ start.py
@@ -9,37 +9,37 @@ from src.ccu.CCU import CCU
 # Start server:
 main()
 
-# Generate average data report:
-report = Report()
-avg_report = report.average("Data.csv")
-avg_report.generate_report("Average_data_report.pdf")
+# # Generate average data report:
+# report = Report()
+# avg_report = report.average("Data.csv")
+# avg_report.generate_report("Average_data_report.pdf")
 
-# Generate patient report:
-rep = Report()
-patient_rep = rep.patient("Data.csv")
-patient_rep.generate_patient_data_report(2906, "Patient_Report.pdf")
+# # Generate patient report:
+# rep = Report()
+# patient_rep = rep.patient("Data.csv")
+# patient_rep.generate_patient_data_report(2906, "Patient_Report.pdf")
 
-# Access Report Generation through CCU:
-ccu = CCU()
-report = ccu.reports()
-avg_report = report.average("Data.csv")
-avg_report.generate_report("Average_data_report.pdf")
-patient_rep = report.patient(2906, "Data.csv")
-patient_rep.generate_patient_report("Patient_Report.pdf")
+# # Access Report Generation through CCU:
+# ccu = CCU()
+# report = ccu.reports()
+# avg_report = report.average("Data.csv")
+# avg_report.generate_report("Average_data_report.pdf")
+# patient_rep = report.patient(2906, "Data.csv")
+# patient_rep.generate_patient_report("Patient_Report.pdf")
 
-# Fetch Patient's Dietary Requirements Data:
-patient = Patient("Data.csv", 2956)
-dietary_data = patient.dietary_req()
-data = dietary_data.fetch_dietary_req_data()
-print(data)
+# # Fetch Patient's Dietary Requirements Data:
+# patient = Patient("Data.csv", 2956)
+# dietary_data = patient.dietary_req()
+# data = dietary_data.fetch_dietary_req_data()
+# print(data)
 
-# Fetch Patient's Respiratory Measurements Data:
-patient = Patient("Data.csv", 2956)
-resp_data = patient.resp_measurments()
-data = resp_data.fetch_resp_measurements_data()
-print(data)
+# # Fetch Patient's Respiratory Measurements Data:
+# patient = Patient("Data.csv", 2956)
+# resp_data = patient.resp_measurments()
+# data = resp_data.fetch_resp_measurements_data()
+# print(data)
 
-# Fetch all patient data:
-patient = Patient("Data.csv")
-data = patient.fetch_all_patient_data()
-print(data)
+# # Fetch all patient data:
+# patient = Patient("Data.csv")
+# data = patient.fetch_all_patient_data()
+# print(data)
