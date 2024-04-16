aa3b5b58a739d8773feb0d6bd2c684aa7c1f29b3
Dina Metwalli <dina.moh657@gmail.com>
2024-04-16 14:32:45 +0100

    feat(electron): update setup page to handle errors

--- electron/setup.js
+++ electron/setup.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const setupPasskeysButton = document.getElementById('setup');
-    const inputPasskeysButton = document.getElementById('entry');
+    const setupPasskeysButton = document.getElementById('setup-btn');
+    const resultElement = document.getElementById('result');
 
     const entryPass = document.getElementById('entry-pass');
     const importPass = document.getElementById('import-pass');
     const exportPass = document.getElementById('export-pass');
-
-    const entryInput = document.getElementById('entry-input');
 
     setupPasskeysButton.addEventListener('click', async () => {
 
         const entry_pass = entryPass.value;
         const import_pass = importPass.value;
         const export_pass = exportPass.value;
+
+        if (entry_pass == "" || import_pass == "" || export_pass == "") {
+            resultElement.textContent = `All fields must be filled in before proceeding.`;
+            return;
+        }
 
         try {
             const response = await fetch('http://localhost:6002/setup', {
@@ -31,30 +34,10 @@ document.addEventListener('DOMContentLoaded', () => {
             const data = await response.json();
             console.log('Response:', data);
 
+            window.location.href = 'login.html';
+
         } catch (error) {
             console.error('Error setting up passkeys:', error);
-        }
-    });
-
-    inputPasskeysButton.addEventListener('click', async () => {
-
-        const entry_password = entryInput.value;
-
-        try {
-            const response = await fetch('http://localhost:6002/auth-entry', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({ entry_password })
-            });
-
-            if (!response.ok) {
-                throw new Error('Failed to generate report');
-            }
-
-        } catch (error) {
-            console.error('Error generating report:', error);
         }
     });
 });
