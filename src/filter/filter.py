eafefa816cde499288607932d13d41b3953891ca
Dina Metwalli <dina.moh657@gmail.com>
2024-04-17 07:44:49 +0100

    fix(filters): filter by referral status data type

--- src/filter/filter.py
+++ src/filter/filter.py
@@ -4,27 +4,44 @@ import pandas as pd
 
 class Filter:
     def __init__(self):
-        self.patients = None
-        self.parse_csv = ParseCSV()
+        self.patients_data = None
+        self.parser = ParseCSV()
 
-    def filter(self):
+    def filter(self, csv_path):
         # Import CSV data
-        self.parse_csv.import_csv()
-        self.patients = self.parse_csv.data
+        self.patients_data = self.parser.import_csv(csv_path)
 
-    def filter_by_referral(self, referral):
-        if self.patients is None or len(self.patients) == 0:
-            self.filter()
-        # Convert the 'referral' column to float for comparison
-        self.patients['referral'] = self.patients['referral'].astype(float)
-        filtered_data = self.patients[self.patients['referral'] == referral]
-        return filtered_data
+    def filter_by_referral(self, referral, csv_path):
+        if self.patients_data is None or len(self.patients_data) == 0:
+            self.filter(csv_path)
+
+        if referral is not False:
+            status = 1.0
+        else:
+            status = 0.0
+
+        print(status)
+        
+        filtered_patients_data = []
+
+        # Convert 'referral' column to float
+        self.patients_data['referral'] = self.patients_data['referral'].astype(float)
+        filtered_data = self.patients_data[self.patients_data['referral'] == status]
+    
+        if not filtered_data.empty:
+            for index, row in filtered_data.iterrows():
+                patient_data = {}
+                for column in self.patients_data.columns:
+                    patient_data[column] = row[column]
+                filtered_patients_data.append(patient_data)
+
+        return filtered_patients_data
 
     def filter_by_bmi(self, bmi):
-        if self.patients is None or len(self.patients) == 0:
+        if self.patients_data is None or len(self.patients_data) == 0:
             self.filter()
         # Filter out empty strings in 'bmi' column
-        filtered_data = self.patients[self.patients['bmi'].astype(str).str.strip() != '']
+        filtered_data = self.patients_data[self.patients_data['bmi'].astype(str).str.strip() != '']
         filtered_data['bmi'] = pd.to_numeric(filtered_data['bmi'], errors='coerce')  # Convert to numeric, coerce errors
         filtered_data = filtered_data[round(filtered_data['bmi']) == round(bmi)]
         return filtered_data
